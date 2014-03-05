5c5b8a7b49b4888015686d5832a494fa2cf1c07a
ifcunharro <ifcunharro@gmail.com>
2014-03-05 12:21:48 +0100

    Change redirect

--- src/test/java/es/uvigo/esei/daa/mysql-tests.sql
+++ src/test/java/es/uvigo/esei/daa/mysql-tests.sql
@@ -23,19 +23,17 @@ CREATE TABLE `users` (
 	PRIMARY KEY (`login`)
 );
 
-INSERT INTO `people` (`id`,`name`,`surname`) VALUES (0,'AntÃ³n','Ã�lvarez');
-INSERT INTO `people` (`id`,`name`,`surname`) VALUES (0,'Ana','Amargo');
-INSERT INTO `people` (`id`,`name`,`surname`) VALUES (0,'Manuel','MartÃ­nez');
-INSERT INTO `people` (`id`,`name`,`surname`) VALUES (0,'MarÃ­a','MÃ¡rquez');
-INSERT INTO `people` (`id`,`name`,`surname`) VALUES (0,'Lorenzo','LÃ³pez');
-INSERT INTO `people` (`id`,`name`,`surname`) VALUES (0,'Laura','Laredo');
+INSERT INTO `people` (`id`,`name`,`surname`) VALUES (0,'Antón','Pérez');
+INSERT INTO `people` (`id`,`name`,`surname`) VALUES (0,'Manuel','Martínez');
+INSERT INTO `people` (`id`,`name`,`surname`) VALUES (0,'Laura','Reboredo');
 INSERT INTO `people` (`id`,`name`,`surname`) VALUES (0,'Perico','Palotes');
-INSERT INTO `people` (`id`,`name`,`surname`) VALUES (0,'Patricia','PÃ©rez');
-INSERT INTO `people` (`id`,`name`,`surname`) VALUES (0,'Juan','JimÃ©nez');
-INSERT INTO `people` (`id`,`name`,`surname`) VALUES (0,'Julia','Justa');
+INSERT INTO `people` (`id`,`name`,`surname`) VALUES (0,'Ana','María');
+INSERT INTO `people` (`id`,`name`,`surname`) VALUES (0,'María','Nuevo');
+INSERT INTO `people` (`id`,`name`,`surname`) VALUES (0,'Alba','Fernández');
+INSERT INTO `people` (`id`,`name`,`surname`) VALUES (0,'Asunción','Jiménez');
+
+INSERT INTO `users` (`login`,`password`) VALUES ('mrjato', '59189332a4abf8ddf66fde068cad09eb563b4bd974f7663d97ff6852a7910a73');
 
 INSERT INTO `state` (`id`,`street`,`number`,`locality`,`province`) VALUES (0,'Principal',21,'Lalin','Pontevedra');
 INSERT INTO `state` (`id`,`street`,`number`,`locality`,`province`) VALUES (0,'Joaquin Loriga',5,'Lalin','Pontevedra');
-INSERT INTO `state` (`id`,`street`,`number`,`locality`,`province`) VALUES (0,'Perico',200,'Lol','MilÃ¡n');
--- login: mrjato, password: mrjato
-INSERT INTO `users` (`login`,`password`) VALUES ('mrjato', '59189332a4abf8ddf66fde068cad09eb563b4bd974f7663d97ff6852a7910a73');
+INSERT INTO `state` (`id`,`street`,`number`,`locality`,`province`) VALUES (0,'Perico',200,'Lol','Milán');
--- src/test/java/es/uvigo/esei/daa/web/StateWebTest.java
+++ src/test/java/es/uvigo/esei/daa/web/StateWebTest.java
@@ -12,7 +12,6 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 //puesto por mi este chrome
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
